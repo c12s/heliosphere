8e961737ef31e2bc670eed624373440dc3ad1d75
Ivana Kovacevic <ivana.kovacevic@ftninformatika.com>
2024-04-22 21:45:49 +0200

    Dodati status kodovi za rl. Promenjen nacin provere rl-a.

--- rate-limiter-service/store/ratelimiterstore.go
+++ rate-limiter-service/store/ratelimiterstore.go
@@ -3,20 +3,23 @@ package store
 import (
 	"context"
 	"fmt"
-	"time"
-
+	"log"
 	"rate-limiter-service/config"
 	pb "rate-limiter-service/proto/ratelimiter"
+	"time"
 
 	"github.com/RussellLuo/slidingwindow"
 	"github.com/hashicorp/consul/api"
 	"go.uber.org/ratelimit"
 	"golang.org/x/time/rate"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/proto"
 )
 
 type RateLimiterStore struct {
-	cli *api.Client
+	cli          *api.Client
+	rateLimiters map[string]interface{}
 }
 
 func New() (*RateLimiterStore, error) {
@@ -32,7 +35,8 @@ func New() (*RateLimiterStore, error) {
 	}
 
 	return &RateLimiterStore{
-		cli: client,
+		cli:          client,
+		rateLimiters: make(map[string]interface{}),
 	}, nil
 }
 
@@ -42,14 +46,15 @@ func (rs *RateLimiterStore) Get(ctx context.Context, id string) (*pb.RateLimiter
 	key := fmt.Sprintf("rateLimiters/%s", id)
 
 	pair, _, err := kv.Get(key, nil)
-	if err != nil {
-		return nil, err
+	if err != nil || pair == nil {
+		return nil, status.Error(codes.NotFound, fmt.Sprintf("Ratelimiter %s not found.", id))
 	}
 
 	rateLimiter := &pb.RateLimiter{}
+
 	err = proto.Unmarshal(pair.Value, rateLimiter)
 	if err != nil {
-		return nil, err
+		return nil, status.Error(codes.NotFound, fmt.Sprintf("Ratelimiter %s cannot be unmarshaled.", id))
 	}
 
 	return rateLimiter, nil
@@ -81,7 +86,7 @@ func (rs *RateLimiterStore) Create(ctx context.Context, limiter *pb.CreateRateLi
 	kv := rs.cli.KV()
 
 	id := ""
-	if limiter.RateLimiter.Name == "sistem" {
+	if limiter.RateLimiter.Name == "system" {
 		id = fmt.Sprintf("%s", limiter.RateLimiter.Name)
 	} else {
 		id = fmt.Sprintf("%s-%s", limiter.RateLimiter.Name, limiter.RateLimiter.UserName)
@@ -92,13 +97,13 @@ func (rs *RateLimiterStore) Create(ctx context.Context, limiter *pb.CreateRateLi
 
 	data, err := proto.Marshal(limiter.RateLimiter)
 	if err != nil {
-		return nil, err
+		return nil, status.Error(codes.InvalidArgument, fmt.Sprintf("Ratelimiter %s cannot be created.", id))
 	}
 
 	p := &api.KVPair{Key: key, Value: data}
 	_, err = kv.Put(p, nil)
 	if err != nil {
-		return nil, err
+		return nil, status.Error(codes.InvalidArgument, fmt.Sprintf("Ratelimiter %s cannot be created.", id))
 	}
 
 	return limiter.RateLimiter, nil
@@ -114,14 +119,17 @@ func (rs *RateLimiterStore) Update(ctx context.Context, limiter *pb.UpdateRateLi
 
 	data, err := proto.Marshal(limiter.RateLimiter)
 	if err != nil {
-		return nil, err
+		return nil, status.Error(codes.InvalidArgument, fmt.Sprintf("Ratelimiter %s cannot be updated.", id))
 	}
 
 	p := &api.KVPair{Key: key, Value: data}
 	_, err = kv.Put(p, nil)
 	if err != nil {
-		return nil, err
+		return nil, status.Error(codes.InvalidArgument, fmt.Sprintf("Ratelimiter %s cannot be updated.", id))
 	}
+
+	//update current state in map
+	rs.createOrUpdateLimiter(limiter.RateLimiter, true)
 
 	return limiter.RateLimiter, nil
 }
@@ -133,8 +141,11 @@ func (rs *RateLimiterStore) Delete(ctx context.Context, id string) (*pb.DeleteRa
 
 	_, err := kv.Delete(key, nil)
 	if err != nil {
-		return nil, err
+		return nil, status.Error(codes.NotFound, fmt.Sprintf("Ratelimiter %s not found.", id))
 	}
+
+	// delete from map
+	delete(rs.rateLimiters, id)
 
 	return &pb.DeleteRateLimiterResponse{
 		Deleted: true,
@@ -144,29 +155,33 @@ func (rs *RateLimiterStore) Delete(ctx context.Context, id string) (*pb.DeleteRa
 func (rs *RateLimiterStore) IsRequestAllowed(ctx context.Context, id string) (*pb.AllowResponse, error) {
 	rateLimiter, err := rs.Get(ctx, id)
 	if err != nil {
-		return nil, err
+		log.Printf("%v", err)
+		return nil, status.Error(codes.NotFound, fmt.Sprintf("Ratelimiter %s not found.", id))
 	}
+
+	rs.createOrUpdateLimiter(rateLimiter, false)
 
 	switch rateLimiter.Type {
 	case "tokenBucket":
-		limiter := rate.NewLimiter(rate.Limit(rateLimiter.ReqPerSec), int(rateLimiter.Burst))
-		allowed := limiter.Allow()
-		return &pb.AllowResponse{
-			Allowed: allowed,
-		}, nil
+		limiter, ok := rs.rateLimiters[rateLimiter.Id].(*rate.Limiter)
+		if ok {
+			log.Printf("broj dostupnih tokena: %f", limiter.Tokens())
+			allowed := limiter.Allow()
+			log.Printf("%v", allowed)
+			return &pb.AllowResponse{
+				Allowed: allowed,
+			}, nil
+		}
+		return nil, fmt.Errorf("ne moz da se preuzme limiter iz mape")
 	case "leakyBucket":
-		limiter := ratelimit.New(int(rateLimiter.ReqPerSec))
+		limiter := rs.rateLimiters[rateLimiter.Id].(ratelimit.Limiter)
 		limiter.Take()
 		allowed := true
 		return &pb.AllowResponse{
 			Allowed: allowed,
 		}, nil
 	case "slidingWindow":
-		limiter, _ := slidingwindow.NewLimiter(time.Second, rateLimiter.ReqPerSec, func() (slidingwindow.Window, slidingwindow.StopFunc) {
-			// NewLocalWindow returns an empty stop function, so it's
-			// unnecessary to call it later.
-			return slidingwindow.NewLocalWindow()
-		})
+		limiter := rs.rateLimiters[rateLimiter.Id].(*slidingwindow.Limiter)
 		allowed := limiter.Allow()
 		return &pb.AllowResponse{
 			Allowed: allowed,
@@ -176,4 +191,33 @@ func (rs *RateLimiterStore) IsRequestAllowed(ctx context.Context, id string) (*p
 		return nil, fmt.Errorf("algorithm is not implemented")
 	}
 
+}
+
+func (rs *RateLimiterStore) createOrUpdateLimiter(rateLimiter *pb.RateLimiter, forUpdate bool) error {
+	var limiter interface{}
+
+	if forUpdate && rs.rateLimiters[rateLimiter.Id] == nil {
+		return fmt.Errorf("Rate limiter not found")
+	}
+
+	// already in map and no need for update
+	if !forUpdate && rs.rateLimiters[rateLimiter.Id] != nil {
+		log.Println("already in map, skip")
+		return nil
+	}
+
+	switch rateLimiter.Type {
+	case "tokenBucket":
+		limiter = rate.NewLimiter(rate.Limit(rateLimiter.ReqPerSec), int(rateLimiter.Burst))
+	case "leakyBucket":
+		limiter = ratelimit.New(int(rateLimiter.ReqPerSec))
+	case "slidingWindow":
+		limiter, _ = slidingwindow.NewLimiter(time.Second, rateLimiter.ReqPerSec, func() (slidingwindow.Window, slidingwindow.StopFunc) {
+			return slidingwindow.NewLocalWindow()
+		})
+	}
+
+	rs.rateLimiters[rateLimiter.Id] = limiter
+
+	return nil
 }
